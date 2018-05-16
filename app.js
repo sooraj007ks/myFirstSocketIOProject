d74e40d1a52a48721b0b66a27525798b7e834462
Sooraj K S <sooraj007ks@gmail.com>
2018-05-16 21:13:25 +0530

    added new msg and create msg functionality

--- app.js
+++ app.js
@@ -15,13 +15,33 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket)=>{
     console.log("New user connected");
-    
+
+    // socket.emit('newEmail', {
+    //     from:"mike@example.com",
+    //     text:"Hello",
+    //     createdAt: new Date()
+    // });
+
+    socket.emit('newMsg', {
+        from:"testmsg@example.com",
+        text:"Hello, This is new msg from server",
+        createsAt: new Date()
+    });
+
+    socket.on('newMsgFromClient', (msg)=>{
+        console.log("New msg created by client", msg);
+    });
+
+    // socket.on('createEmail', (newEmail)=>{
+    //     console.log('createEmail', newEmail);
+    // });
+
     socket.on('disconnect', ()=>{
         console.log("Client Disconnected");
+    });
 });
 
 
-});
 
 
 server.listen(port, ()=> console.log(`Server started at ${port}`));
