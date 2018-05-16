1a58aaf469e68ac0d9c4793194cda365f9b1b258
Sooraj K S <sooraj007ks@gmail.com>
2018-05-16 21:15:53 +0530

    JS directory

---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+let socket = io();
+
+socket.on('connect', function(){
+    console.log("Connected to Server");
+    // socket.emit('createEmail', {
+    //     from:"client@example.com",
+    //     text:"Hello",
+    //     createdAt: new Date()
+    // });
+
+    socket.emit('newMsgFromClient', {
+        from:"newmsg@gmail.com",
+        text:"hey there"
+    });
+});
+
+socket.on('disconnect', function(){
+    console.log("Disconnected from server");
+});
+
+// socket.on('newEmail', function(emailData){
+//     console.log("New email", emailData, emailData.from);
+// });
+
+socket.on('newMsg', function(msg){
+    console.log("A new message received", msg);
+});
